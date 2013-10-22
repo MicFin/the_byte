55d4824a93f61cbffd0b6b1a8543d51e890386c4
Michael Finneran <mrfinneran@gmail.com>
2013-10-22 19:39:55 -0400

    created rails file and CreateUsers model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheByte::Application.config.secret_key_base = 'c57b2d7f4ac5e94577dc17a12d446342f78d23985110148204098562397d6a14d9d1c5ae35f74518250b46a8568d6dbc35da38e3023b55bd7f262773b6113e49'
