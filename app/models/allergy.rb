d6661d24aa7718b7f080884a9ef99a033f18ca66
Michael Finneran <mrfinneran@gmail.com>
2013-10-22 19:52:21 -0400

    set up relationships between models

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,2 +1,3 @@
 class Allergy < ActiveRecord::Base
+  has_and_belongs_to_many :members
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ActiveRecord::Base
+  belongs_to :user
+  has_and_belongs_to_many :allergies
+  has_and_belongs_to_many :diets
+  has_and_belongs_to_many :excludedingredients
 end
